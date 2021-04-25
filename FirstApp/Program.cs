78d91ba5712228f9192f462f3040b29b0320ffa0
SuperSerZh <serzhik05@mail.ru>
2021-04-25 14:44:22 +0300

    Изменил вывод на экран "Yes"

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -5,11 +5,15 @@ class MainClass
 	public static void Main(string[] args)
 	{
 
-		string myName;
-		myName = "SerZh";
+		string MyName = "Jane";
+		byte MyAge = 27;
+		bool HaveIApet = true;
+		double MyShoeSize = 37.5;
 
-		Console.WriteLine(myName);
+		Console.WriteLine("My name is " + MyName);
+		Console.WriteLine("MyAge " + MyAge);
+		Console.WriteLine("Do I have a pet? " + (HaveIApet? "Yes":"No"));
+		Console.WriteLine("My shoe size is " + MyShoeSize);
 
-		Console.ReadKey();
 	}
 }
